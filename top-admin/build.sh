10233ec64e97c5f084118cb37234a743454f65c7
chenyu <jchen_px@126.com>
2022-01-12 19:03:06 +0800

    打包脚本

---
+++ top-admin/build.sh
@@ -0,0 +1,14 @@
+version="v1.0.0"
+if [ -n "$1" ]; then
+    version=$1
+fi
+
+rm -rf dist
+
+npm run build 
+
+docker buildx build -t top-admin:$version .
+docker tag top-admin:$version 1633182434/top-admin:$version
+docker push 1633182434/top-admin:$version
+docker rmi top-admin:$version
+docker rmi 1633182434/top-admin:$version
